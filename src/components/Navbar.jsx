5f570b3cdbc499f637752560ce93dc8eb42b97ce
henryxddofficial <henryng1219@gmail.com>
2022-05-27 07:17:21 -0400

    third update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           <button className="border-none bg-transparent text-black mr-4">
             About Me
           </button>
-          <button className="px-8 py-3">Contant</button>
+          <button className="px-8 py-3">Contact</button>
         </div>
         <div className="md:hidden" onClick={handleClick}>
         {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' />}
